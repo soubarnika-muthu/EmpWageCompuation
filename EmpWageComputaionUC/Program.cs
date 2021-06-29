ef4daf069f1f948f31b5fc5b1d82b78b12a6c530
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-06-29 17:51:15 +0530

    [soubarnika]Added UC2 program

--- EmpWageComputaionUC/Program.cs
+++ EmpWageComputaionUC/Program.cs
@@ -8,18 +8,26 @@ namespace EmployeeWagecomputaion
         {
             ////Creating object or Instance of random class
             Random random = new Random();
-            //Initializing variables
+            //iNitializing variable
+            int EMP_WAGE = 0;
             int EMP_PRESENT = 1;
             int EMP_INPUT = random.Next(0, 2);
-            //Using if condtion to check Employee attendance
+            int FULL_TIME = 0;
+            int WAGE_PER_HR = 20;
+            //Using If condition checking whether employee is present or not 
+
             if (EMP_INPUT == EMP_PRESENT)
             {
                 Console.WriteLine("Employee is Present");
+                FULL_TIME = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
             }
+            ////Calculating Daily Wages of Employee
+            EMP_WAGE = FULL_TIME * WAGE_PER_HR;
+            Console.WriteLine("Daily Employee Wage is" + EMP_WAGE);
         }
     }
 }
