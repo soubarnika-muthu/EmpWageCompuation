435c56b802cb288226bbfecd84e51d9c0f274699
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-06-29 17:27:43 +0530

    [soubarnika]Added UC1 program

--- EmpWageComputaionUC/Program.cs
+++ EmpWageComputaionUC/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace EmpWageComputaionUC
+namespace EmployeeWagecomputaion
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION!");
+            ////Creating object or Instance of random class
+            Random random = new Random();
+            //Initializing variables
+            int EMP_PRESENT = 1;
+            int EMP_INPUT = random.Next(0, 2);
+            //Using if condtion to check Employee attendance
+            if (EMP_INPUT == EMP_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
