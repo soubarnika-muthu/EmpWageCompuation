cd12c1f744568d14576d39b0284eb7234e80db29
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-03 14:43:36 +0530

    [soubarnika]Added UC8 Program

--- EmpWageComputaionUC/Program.cs
+++ EmpWageComputaionUC/Program.cs
@@ -9,23 +9,24 @@ namespace EmployeeWagecomputaion
         //Adding constant global variable
         const int FULL_TIME = 1;
         const int PART_TIME = 2;
-        const int EMP_RATE_PER_HR = 20;
+        
 
-        public static int ComputeEmpWage()
+        public static int ComputeEmpWage(String COMPANY_NAME,int EMP_RATE_PER_HR, int NUM_OF_WORKINGDAYS,int MAX_HRS_PER_MONTH )
         {
           //Initialize local variable
             int EMP_HRS = 0;
-            int EMP_WAGES;
+            int TOTAL_HRS = 0;
+            int TOTAL_WORKINGDAYS = 0;
 
-            int WORKING_HRS = 1;
-            int WORKING_DAYS = 1;
+           
+           
             int TOTAL_WAGES = 0;
 
             //Creating object or Instance of random class
             Random random = new Random();
 
             //Using for loop to calculate wages for 20 days
-            while (WORKING_HRS <= 100 && WORKING_DAYS <= 20)
+            while (TOTAL_HRS <=MAX_HRS_PER_MONTH  && TOTAL_WORKINGDAYS <=NUM_OF_WORKINGDAYS)
             {
                 //Generating Random Value by Calling Next Method
                 int EMP_INPUT = random.Next(0, 3);
@@ -44,22 +45,23 @@ namespace EmployeeWagecomputaion
                         EMP_HRS = 0;
                         break;
                 }
+                TOTAL_HRS += EMP_HRS;
+               
+                TOTAL_WORKINGDAYS++;
 
-
-                //Calculating Daily Wages of Employee
-                EMP_WAGES = EMP_HRS * EMP_RATE_PER_HR;
-                WORKING_HRS += EMP_HRS;
-                TOTAL_WAGES += EMP_WAGES;
-                WORKING_DAYS++;
+              
             }
-
-
-            Console.WriteLine("Employee wage for " + WORKING_DAYS + " day is " + TOTAL_WAGES);
+            //Calculating Daily Wages of Employee
+            TOTAL_WAGES = TOTAL_HRS * EMP_RATE_PER_HR;
+            Console.WriteLine("Total Employee wage for Company " + COMPANY_NAME + " is " + TOTAL_WAGES);
             return TOTAL_WAGES;
         }
         static void Main(String[] args)
         {
-            ComputeEmpWage();
+            ComputeEmpWage("TVSNEXT",20,10,10);
+            ComputeEmpWage("HCL", 20, 10, 10);
+            ComputeEmpWage("TCS", 20, 10, 10);
+
         }
     }
 }
